753c639b8cd4a2d12cc6f46b78445d126f99f7b8
Kody Dahl <kody.dahl@gmail.com>
2026-04-10 11:11:35 -0400

    Adding a few reviews

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -11,7 +11,7 @@ export const reviews: Review[] = [
     stars: 5,
   },
   {
-    name: "Kelly Payton",
+    name: "Kelly P.",
     text: "I love Othermilk! I've been getting one bottle a week and making protein shakes with it, it's the perfect non dairy option with no bad stuff in it. And Liz is the best! She really cares about sourcing quality ingredients and puts so much care into this product, and it shows!",
     stars: 5,
   },
@@ -23,6 +23,21 @@ export const reviews: Review[] = [
   {
     name: "Kody D.",
     text: "milk made from pecans is a dream come true. absolutely astonishing, decadent product. everything's better with the plain pecan in it, and the chocolate pecan will blow your mind. I love both in my protein shakes!",
+    stars: 5,
+  },
+  {
+    name: "Logan L.",
+    text: "Liz‘s milk product is above and beyond the best non-dairy milk alternative I have ever purchased!! She sources her pecans from a local farmer, uses only properly filtered water,  and recycles her glass bottles! The subtle pecan flavor is really nice and it’s so creamy!  Her chocolate milk is also amazing and not overly sweetened! 10/10 recommend!",
+    stars: 5,
+  },
+  {
+    name: "Matt C.",
+    text: "Delicious, sustainable, and clean milk alternatives! Liz goes above and beyond!",
+    stars: 5,
+  },
+  {
+    name: "Tessa R.",
+    text: "Y U M! I love everything about this local company and their products. From the delicious alternative milk flavors to the quality, truly nothing compares. I love that more local businesses are supplying and incorporating Other milks into their drinks, too!",
     stars: 5,
   },
 ];
